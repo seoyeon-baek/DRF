ccd0dfb8c24aee6874d2a6d258a69f23f2bd940d
seoyeon-baek <w2032@e-mirim.hs.kr>
2022-04-22 13:57:24 +0900

    Add: router connect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,30 @@
+import React from "react";
+import {Route} from "react-router";
 import "./App.css";
+import Nav from "./components/Nav";
+import Header from "./components/Header";
+import LoginModal from "./components/LoginModal";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="App">
+        <div className="auto-margin">
+          <Route exact path="/">
+            <Header modal={modal} />
+          </Route>
+
+          <Route exact path="/">
+            <Nav />
+          </Route>
+
+          <Route exact path="/login">
+            <LoginModal setModal={setModal} />
+          </Route>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
